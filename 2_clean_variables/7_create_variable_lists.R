26f4c5016d131dee86efc10e2a43f031a1b26afc
boyercb <christopherbboyer@gmail.com>
2019-08-21 23:56:16 -0400

    Create 7_create_variable_lists.R

---
+++ 2_clean_variables/7_create_variable_lists.R
@@ -0,0 +1,22 @@
+primary_outcomes <- c(
+  "fc_use_ever",
+  "fc_use_6mo",
+  "fc_use_last",
+  "any_use_ever",
+  "any_use_6mo",
+  "any_use_last",
+  "fc_try"
+)
+
+secondary_outcomes <- c(
+  "cont_z_know",
+  "fc_recognize",
+  "mc_z_opinion",
+  "fc_z_opinion",
+  "cont_discussed"
+)
+
+outcomes <- c(
+  primary_outcomes,
+  secondary_outcomes
+)
