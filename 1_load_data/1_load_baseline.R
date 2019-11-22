0a48112f95af530d4f761870beba31c79cb7db22
boyercb <christopherbboyer@gmail.com>
2019-11-22 09:08:07 -0500

    Update 1_load_baseline.R

--- 1_load_data/1_load_baseline.R
+++ 1_load_data/1_load_baseline.R
@@ -13,7 +13,8 @@ bl <- bl %>%
   mutate(
     any_use_ever = mc_use_ever == 1 | fc_use_ever == 1,
     any_use_6mo = mc_use_6mo == 1 | fc_use_6mo == 1,
-    any_use_last = mc_use_last == 1 | fc_use_last == 1
+    any_use_last = mc_use_last == 1 | fc_use_last == 1,
+    ward = as.numeric(ward)
   ) %>%
   mutate_at(vars(starts_with("any_use")), as.numeric)
 
